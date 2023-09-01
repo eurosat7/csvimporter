bababcec6888b662b5af01f42b14d8adf7a857af
eurosat7 <eurosat7@yahoo.de>
2023-09-01 20:32:58 +0200

    measuring memory usage

--- src/CsvImportController.php
+++ src/CsvImportController.php
@@ -15,6 +15,7 @@ class CsvImportController
 
     public function process(?string $filename): void
     {
+        $mem_pre = round(memory_get_peak_usage() / 1024 / 1024 * 100) / 100;
         $lines = $this->fileTools->lineCountOfFile($filename);
         if ($filename === null || $lines === 0) {
             echo "no file was uploaded.", "\r\n";
@@ -25,5 +26,7 @@ class CsvImportController
 
         $imported = $this->fileToEntitysConverter->processFile($filename, $this->entityRepository);
         echo "\r\n", "imported ", $imported, " entities.", "\r\n";
+        $mem_post = round(memory_get_peak_usage() / 1024 / 1024 * 100) / 100;
+        echo "\r\n", "memory usage (in MB) started at ", $mem_pre, " and went as high as ", $mem_post, "\r\n";
     }
 }
