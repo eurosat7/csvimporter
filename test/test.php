3d209bdfab02a09999e5fdcae46d9b8d417a4dee
eurosat7 <eurosat7@yahoo.de>
2023-12-01 15:43:07 +0100

    config array has become an object

--- test/test.php
+++ test/test.php
@@ -4,16 +4,16 @@
 
 declare(strict_types=1);
 
-use Eurosat7\Csvimporter\CsvImportController;
-use Eurosat7\Csvimporter\EntityRepository;
-use Eurosat7\Csvimporter\FileToEntitysConverter;
-use Eurosat7\Csvimporter\FileTools;
+use Eurosat7\Csvimporter\Controller\CsvImportController;
+use Eurosat7\Csvimporter\Database\EntityRepository;
+use Eurosat7\Csvimporter\Services\FileToEntitysConverter;
+use Eurosat7\Csvimporter\Services\FileTools;
 
 $filename = dirname(__DIR__) . "/generated_entities_for_testing.csv";
 if (!file_exists($filename)) {
     die("please run first: make csv");
 }
-$config = include(dirname(__DIR__) . '/bootstrap.php');
+$config = include(dirname(__DIR__) . '/instanceContainer.php');
 $fileTools = new FileTools();
 $entityRepository = new EntityRepository($config["mysqlConnection"]);
 $fileToEntitysConverter = new FileToEntitysConverter();
