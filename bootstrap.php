9ee5d6aa135c20635132e18230495b0152f57acf
eurosat7 <eurosat7@yahoo.de>
2023-12-01 15:40:59 +0100

    moved code outside the public folder

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,30 @@
 <?php
+// should be served by docker at http://localhost:8089/public/
+// > make init
 declare(strict_types=1);
 
-use Eurosat7\Csvimporter\MySqlConnection;
+use Eurosat7\Csvimporter\Controller\CsvImportController;
+use Eurosat7\Csvimporter\Database\EntityRepository;
+use Eurosat7\Csvimporter\InstanceContainer;
+use Eurosat7\Csvimporter\Services\FileToEntitysConverter;
+use Eurosat7\Csvimporter\Services\FileTools;
 
-include(__DIR__ . "/vendor/autoload.php");
+/** @var InstanceContainer $config */
+$config = include(__DIR__ . '/instanceContainer.php');
+$te = $config->templateEngine;
 
-return [
-    'mysqlConnection' => new MySqlConnection(
-        host: "mysql-csvimporter:3306",
-        user: "csvimporter",
-        password: "csvimporterpassword",
-        database: "csv"
-    ),
-];
+// as you can see the router component is missing
+// this code can currently only serve one page calles "index" running the CsvImportController
+
+$fileTools = new FileTools();
+$entityRepository = new EntityRepository($config->mysqlConnection);
+$fileToEntitysConverter = new FileToEntitysConverter();
+$controller = new CsvImportController($entityRepository, $fileTools, $fileToEntitysConverter);
+
+$te->setController($controller);
+$te->page(
+    "index",
+    [
+        'title' => "Upload example"
+    ]
+);
--- public/index.php
+++ public/index.php
@@ -1,40 +1,7 @@
 <?php
-// should be served by docker at http://localhost:8089/public/
-// > make init
-declare(strict_types=1);
+// this file is as empty as possible.
+// should the webserver ever be misconfigured
+// and send this file as plain text to the browser
+// nobody will gail any information or code :)
 
-use Eurosat7\Csvimporter\CsvImportController;
-use Eurosat7\Csvimporter\EntityRepository;
-use Eurosat7\Csvimporter\FileToEntitysConverter;
-use Eurosat7\Csvimporter\FileTools;
-
-$config = include(dirname(__DIR__) . '/bootstrap.php');
-$fileTools = new FileTools();
-$entityRepository = new EntityRepository($config["mysqlConnection"]);
-$fileToEntitysConverter = new FileToEntitysConverter();
-$controller = new CsvImportController($entityRepository, $fileTools, $fileToEntitysConverter);
-
-?><!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8"/>
-    <title>upload test</title>
-    <link rel="stylesheet" href="assets/minimum.css"/>
-</head>
-<body>
-<h1>Upload example</h1>
-<form method="post" enctype="multipart/form-data">
-    <div class="form-group">
-        <label for="file">csv file</label>
-        <input type="file" name="file"/>
-    </div>
-    <div class="form-group">
-        <input type="submit" value="upload and import"/>
-    </div>
-</form>
-<pre><?php $controller->process($_FILES['file']['tmp_name'] ?? null); ?></pre>
-<h2>debug</h2>
-<pre>post_max_size: <?php echo ini_get("post_max_size"); ?></pre>
-<pre>upload_max_filesize: <?php echo ini_get("upload_max_filesize"); ?></pre>
-</body>
-</html>
+include(dirname(__DIR__, 1) . "/bootstrap.php");
