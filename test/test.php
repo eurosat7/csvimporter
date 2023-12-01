d11523b663e038435a06142cebeecdc0093e7661
eurosat7 <eurosat7@yahoo.de>
2023-12-01 16:08:19 +0100

    fixed test

--- test/test.php
+++ test/test.php
@@ -6,16 +6,21 @@ declare(strict_types=1);
 
 use Eurosat7\Csvimporter\Controller\CsvImportController;
 use Eurosat7\Csvimporter\Database\EntityRepository;
+use Eurosat7\Csvimporter\InstanceContainer;
 use Eurosat7\Csvimporter\Services\FileToEntitysConverter;
 use Eurosat7\Csvimporter\Services\FileTools;
 
+include(dirname(__DIR__) . "/vendor/autoload.php");
+
 $filename = dirname(__DIR__) . "/generated_entities_for_testing.csv";
 if (!file_exists($filename)) {
-    die("please run first: make csv");
+    die("please run first: make docker-php-csv");
 }
+/** @var InstanceContainer $config */
 $config = include(dirname(__DIR__) . '/instanceContainer.php');
 $fileTools = new FileTools();
-$entityRepository = new EntityRepository($config["mysqlConnection"]);
+$entityRepository = new EntityRepository($config->mysqlConnection);
 $fileToEntitysConverter = new FileToEntitysConverter();
 $controller = new CsvImportController($entityRepository, $fileTools, $fileToEntitysConverter);
-$controller->process($filename);
+$controller->setFile($filename);
+$controller->process();
