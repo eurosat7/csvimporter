19cf24d3a5ac9c6c9945a0fc4b614982cb270d06
eurosat7 <eurosat7@yahoo.de>
2024-03-08 14:04:08 +0100

    Added phpinsights and pdepend. Did some refactoring after feedback; some class names were unprecise. Increased code quality.

--- src/InstanceContainer.php
+++ src/InstanceContainer.php
@@ -1,17 +1,18 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Eurosat7\Csvimporter;
 
-use Eurosat7\Csvimporter\Database\MySqlConnection;
+use Eurosat7\Csvimporter\Database\Handler\DatabaseHandler;
+use Eurosat7\Csvimporter\Database\Handler\HasTransaction;
 use Eurosat7\Csvimporter\Services\TemplateEngine;
 
 readonly class InstanceContainer
 {
     public function __construct(
-        public MySqlConnection $mysqlConnection,
-        public TemplateEngine  $templateEngine
-    )
-    {
+        public DatabaseHandler&HasTransaction $databaseHandler,
+        public TemplateEngine $templateEngine
+    ) {
     }
 }
