f04e7c75a843be6d10ab565b3063e1194d9eae0e
eurosat7 <p0stst4ti0n-github@yahoo.de>
2024-05-25 03:23:51 +0200

    Update index.php
    typo

--- public/index.php
+++ public/index.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 // this file is as empty as possible.
 // should the webserver ever be misconfigured
 // and send this file as plain text to the browser
-// nobody will gail any information or code :)
+// nobody will get any information or useful code :)
 
 require dirname(__DIR__, 1) . '/bootstrap.php';
